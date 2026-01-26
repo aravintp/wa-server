1e1e4de4b8ac3374f00b6b4e0503262f4d12750a
aravintp <36691872+aravintp@users.noreply.github.com>
2026-01-26 23:17:57 +0800

    Changed Docker

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ RUN apk add --no-cache python3 g++ make
 WORKDIR /app
 COPY . .
 RUN yarn install --production
-CMD ["node", "/app/src/index.js"]
+CMD ["node", "/WA-Server/index.js"]
--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const whatsapp = require("./Wa-module"); // Import the app instance
 
 
 app.put('/send-wa-notification', (req, res) => {
-    res.send('Done')
     msg = req.query.msg
     num = req.query.number  + '@c.us'
-    console.log(msg,num)
     whatsapp.sendMessage(num,msg)
+    
+    res.send('Done')
 })
